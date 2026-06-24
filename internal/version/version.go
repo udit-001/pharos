bd14c4c7de1392b15488d6ccf5a57bc8b439ad27
Udit <23015406+udit-001@users.noreply.github.com>
2026-06-25 03:50:57 +0530

    chore: bump version to 0.2.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "runtime/debug"
 
 // Version is the current version of the Pharos CLI.
 // Overridden at build time via ldflags, or detected from Go module info.
-var Version = "0.1.0"
+var Version = "0.2.0"
 
 func init() {
 	info, ok := debug.ReadBuildInfo()
